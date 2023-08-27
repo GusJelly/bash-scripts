7a59a19621847c2a6e5d47c32a64c0577b275a61
Gustavo Meireles <gugatpm@gmail.com>
2023-08-27 18:15:49 +0100

    New tweaks

--- fzf-custom.sh
+++ fzf-custom.sh
@@ -3,12 +3,26 @@
 
 # change current directory with fzf
 conf() {
+   current=$(pwd)
    cd $HOME/.config
-   cd $(find . -maxdepth 1 -type d -print | fzf --reverse --color=16 --preview 'ls {}')
+   newCd=$(find . -maxdepth 1 -type d -print | fzf --reverse --color=16 --preview 'ls {}')
+
+   if [ -z $newCd ]; then
+      cd $current
+   else
+      cd $newCd
+   fi
 }
 
 # Project picker
 proj() {
+   current=$(pwd)
    cd $HOME/projects
-   cd $(find . -maxdepth 1 -type d -print | fzf --reverse --color=16 --preview 'ls {}')
+   newCd=$(find . -maxdepth 2 -type d -print | fzf --reverse --color=16 --preview 'ls {}')
+
+   if [ -z $newCd ]; then
+      cd $current
+   else
+      cd $newCd
+   fi
 }
